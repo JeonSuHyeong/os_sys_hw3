db778ab1a849ab5f5652d75f700e33200baeb517
Nael2m <64085393+Nael2m@users.noreply.github.com>
2022-05-03 08:44:36 +0900

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -20,81 +20,85 @@ int main(int argc, char *argv[])  {
         pthread_t dep[100], wit[100];
         int retval;
         int l;
-
         if (argc <= 1) {
-                printf("Usage:");
+                printf("You should input 3 argument to implement\n");
+                printf("Try again. ./a.out {balance} {MaxTry} {MaxThreads}");
                 exit(1);
         }
         if (argc >= 2) {
                 balance = atoi(argv[1]);
                 maxTry = atoi(argv[2]);
         }
+
         if (argc >3){
                 maxThreads = atoi(argv[3]);
-                printf("Start Balance -> %ld, MaxTry -> %ld, MaxThreads -> %d\n", balance, maxTry, maxThreads);
+                printf("Start Balance -> %ld, MaxTry -> %ld, MaxThreads -> %ld\n", balance, maxTry, maxThreads);
         }
         /**/
-        retval = pthread_mutex_init(&lck, NULL);
+        retval = pthread_mutex_init(&lck, NULL);        //initialize the mutex
         /**/
+
+        //create the pthread to access the global variable.
         for (l=0;l<maxThreads;l++){
                 pthread_create( &dep[l], NULL, deposit, NULL);
                 pthread_create( &wit[l], NULL, withdraw, NULL);
         }
-        
+        //waut the pthread to access the global variable.
         for (l=0;l<maxThreads;l++){
-                pthread_join( &dep[l], NULL);
-                pthread_join( &wit[l], NULL);
+                pthread_join( dep[l], NULL);
+                pthread_join( wit[l], NULL);
         }
-        
-        retval = pthread_mutxex_destroy(&lck);
+        retval = pthread_mutex_destroy(&lck);   //Free the mutex 
 
-        printf("Final Balance -> d%ld\n", balance);
+        printf("Final Balance -> %ld\n", balance);
 
         return 0;
 }
 
 void *deposit(void *args)
 {
-        int i;
+        long i;
+        short ran;
         int *dep = (int *)(args);
-        balance += dep[i];
-        for(i = 0; i < maxTry; ++i)
+
+        for(i = 0; i < maxTry; i++)
         {
                 pthread_mutex_lock(&lck);
-                balance =balance + i;
+                balance = balance + i;
                 balance = swap(balance);
                 pthread_mutex_unlock(&lck);
 
         }
+        t0cnt++;
         pthread_exit(0);
 }
 
 
 void *withdraw(void *args)
 {
-        int i;
+        long i;
         int *wit = (int *)(args);
-        balance -= wit[i];
+
         for(i = 0; i < maxTry; i++)
         {
                 pthread_mutex_lock(&lck);
+                balance -= i;
                 balance = swap(balance);
                 pthread_mutex_unlock(&lck);
 
         }
+        t1cnt++;
         pthread_exit(0);
 }
 
 
 long swap(long x)
 {       
-        int temp_t;
-        temp_t = temp;
-        temp = balance;
-        balance = temp;
-}
-
-void initialize()
-{
-        
+        long tmp;
+        int l;
+        for(l=0; l<100; l++){
+                tmp = x;
+                x = tmp;
+        }
+        return x;
 }
